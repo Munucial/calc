dceec255cdfc119e2d9ca2712852711e7fc05cc8
Andrew <jiganyg@gmail.com>
2017-09-27 20:38:47 +0800

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,1 +1,27 @@
+function plus(nums) {
+  var result = 0;
+  for(i=0; i<nums.length; i++) {
+    result += nums[i];
+  }
+  return result;
+}
 
+function minus(num1, nums) {
+  var result = num1;
+  result -= plus(nums);
+  return result;
+}
+
+function times(nums) {
+  var result = nums[0];
+  for(i=1; i<nums.length; i++) {
+    result *= nums[i];
+  }
+  return result;
+}
+
+function divide(num1, nums) {
+  var result = num1;
+  result /= times(nums);
+  return result;
+}
